eb39e06103d8042b7cd358cc8a929364f16a672e
robertbass <robertbass@hotmail.com>
2025-01-09 10:01:53 -0500

    HEADER AND CART COMPONENTS

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,16 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common'
 import { ProductComponent } from './../../components/product/product.component'
+import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { Product } from '../../../shared/models/product.model'
 
 @Component({
   selector: 'app-list',
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-  formChild(event: String){
-    console.log("Estamos en el padre");
-    console.log(event);
+  products = signal<Product[]>([]);
+  cart = signal<Product[]>([]);
+
+  constructor(){
+    const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Prod 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=23',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Prod 2',
+        price: 150,
+        image: 'https://picsum.photos/640/640?r=20',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Prod 3',
+        price: 75,
+        image: 'https://picsum.photos/640/640?r=13',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Prod 4',
+        price: 60,
+        image: 'https://picsum.photos/640/640?r=33',
+        createdAt: new Date().toISOString()
+      }
+    ];
+    this.products.set(initProducts);
+  }
+
+  addToCart(product: Product){
+    this.cart.update(prevState => [...prevState, product]);
   }
 }
---
+++ src/app/domains/shared/components/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, SimpleChanges, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from '../../models/product.model';
+//import { CartService } from '../../services/cart.service';
+import { RouterLinkWithHref, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, RouterLinkWithHref, RouterLinkActive],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  hideSideMenu = signal(true);
+  @Input({required: true}) cart: Product[] = [];
+  //private cartService = inject(CartService);
+  //cart = this.cartService.cart;
+  //total = this.cartService.total;
+
+  toogleSideMenu() {
+    this.hideSideMenu.update(prevState => !prevState);
+  }
+}
